bb380b6e8b664246a791f553ddc856cbc60dae1f
Angelscry <angelscry74@gmail.com>
2014-12-30 23:11:16 -0500

    Esthetic update related to Kodi/Helix version.

--- addon.py
+++ addon.py
@@ -13,9 +13,8 @@ __author__ = "Angelscry"
 __url__ = "https://github.com/Angelscry/plugin.program.advanced.launcher"
 __git_url__ = "https://github.com/Angelscry/plugin.program.advanced.launcher"
 __credits__ = "Leo212 CinPoU, JustSomeUser, Zerqent, Zosky, Atsumori"
-__version__ = "2.5.7"
+__version__ = "2.5.8"
 
 if ( __name__ == "__main__" ):
     import resources.lib.launcher_plugin as plugin
     plugin.Main()
-
